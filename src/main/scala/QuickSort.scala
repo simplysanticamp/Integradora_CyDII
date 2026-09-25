e2bde6102c72d883fd08003e784f8cbf9bfaedae
Santiago Campo Garcia <santiagocampogarcia@gmail.com>
2026-09-25 17:39:57 -0500

    Add quicksort and tests

--- src/main/scala/QuickSort.scala
+++ src/main/scala/QuickSort.scala
@@ -1,21 +1,64 @@
-class QuickSort {
+import scala.annotation.tailrec
+
+object QuickSort {
+
+  // Elementos de inputList estrictamente menores que p (conserva el orden original)
   def menores(inputList: List[Int], p: Int): List[Int] = {
-
+    @tailrec
+    def aux(l: List[Int], acc: List[Int]): List[Int] = l match {
+      case Nil                  => acc.reverse
+      case h :: t if h < p      => aux(t, h :: acc)
+      case _ :: t               => aux(t, acc)
+    }
+    aux(inputList, Nil)
   }
 
+  // Elementos de inputList mayores o iguales que p (conserva el orden original)
   def mayores(inputList: List[Int], p: Int): List[Int] = {
+    @tailrec
+    def aux(l: List[Int], acc: List[Int]): List[Int] = l match {
+      case Nil                  => acc.reverse
+      case h :: t if h >= p     => aux(t, h :: acc)
+      case _ :: t               => aux(t, acc)
+    }
+    aux(inputList, Nil)
+  }
 
-  }
-  
-  def appendTR(inputL1: List[Int], inputL2: List[Int]):List[Int] = {
-    
+  // Concatena inputL1 ++ inputL2 con recursión de cola
+  def appendTR(inputL1: List[Int], inputL2: List[Int]): List[Int] = {
+    @tailrec
+    def reverso(l: List[Int], acc: List[Int]): List[Int] = l match {
+      case Nil    => acc
+      case h :: t => reverso(t, h :: acc)
+    }
+    @tailrec
+    def pegar(rev1: List[Int], acc: List[Int]): List[Int] = rev1 match {
+      case Nil    => acc
+      case h :: t => pegar(t, h :: acc)
+    }
+    pegar(reverso(inputL1, Nil), inputL2)
   }
 
-  def separar(inputList: List[Int], p:Int, menoresAcc: List[Int], mayoresAcc: List[Int]): (List[Int], List[Int]) = {
-    
+  // Parte inputList en una sola pasada: (menores que p, mayores o iguales que p)
+  // Se llama inicialmente con menoresAcc = Nil y mayoresAcc = Nil
+  @tailrec
+  def separar(
+               inputList: List[Int],
+               p: Int,
+               menoresAcc: List[Int],
+               mayoresAcc: List[Int]
+             ): (List[Int], List[Int]) = inputList match {
+    case Nil              => (menoresAcc.reverse, mayoresAcc.reverse)
+    case h :: t if h < p  => separar(t, p, h :: menoresAcc, mayoresAcc)
+    case h :: t           => separar(t, p, menoresAcc, h :: mayoresAcc)
   }
-  
-  def quickSort(inputList: List[Int]): List[Int] = {
-    
+
+  // Ordena ascendentemente usando la cabeza como pivote
+  def quickSort(inputList: List[Int]): List[Int] = inputList match {
+    case Nil          => Nil
+    case p :: resto   =>
+      val (men, may) = separar(resto, p, Nil, Nil)
+      appendTR(quickSort(men), p :: quickSort(may))
   }
+
 }
--- src/test/scala/QuickSortSuite.scala
+++ src/test/scala/QuickSortSuite.scala
@@ -1,3 +1,42 @@
-class QuickSortSuite {
+class QuickSortSuite extends munit.FunSuite {
+
+  test("menores") {
+    assertEquals(QuickSort.menores(List(), 5), List())
+    assertEquals(QuickSort.menores(List(5), 5), List())
+    assertEquals(QuickSort.menores(List(1, 2, 3, 4, 5), 3), List(1, 2))
+    assertEquals(QuickSort.menores(List(5, 4, 3, 2, 1), 3), List(2, 1))
+    assertEquals(QuickSort.menores(List(1, 2, 2, 3, 3), 2), List(1))
+  }
+
+  test("mayores") {
+    assertEquals(QuickSort.mayores(List(), 5), List())
+    assertEquals(QuickSort.mayores(List(5), 5), List(5))
+    assertEquals(QuickSort.mayores(List(1, 2, 3, 4, 5), 3), List(3, 4, 5))
+    assertEquals(QuickSort.mayores(List(5, 4, 3, 2, 1), 3), List(5, 4, 3))
+    assertEquals(QuickSort.mayores(List(1, 2, 2, 3, 3), 2), List(2, 2, 3, 3))
+  }
+
+  test("appendTR") {
+    assertEquals(QuickSort.appendTR(List(), List()), List())
+    assertEquals(QuickSort.appendTR(List(), List(1, 2, 3)), List(1, 2, 3))
+    assertEquals(QuickSort.appendTR(List(1, 2, 3), List()), List(1, 2, 3))
+    assertEquals(QuickSort.appendTR(List(1, 2), List(3, 4)), List(1, 2, 3, 4))
+  }
+
+  test("separar") {
+    assertEquals(QuickSort.separar(List(), 5, Nil, Nil), (List(), List()))
+    assertEquals(QuickSort.separar(List(5), 5, Nil, Nil), (List(), List(5)))
+    assertEquals(QuickSort.separar(List(1, 2, 3, 4, 5), 3, Nil, Nil), (List(1, 2), List(3, 4, 5)))
+    assertEquals(QuickSort.separar(List(5, 4, 3, 2, 1), 3, Nil, Nil), (List(2, 1), List(5, 4, 3)))
+    assertEquals(QuickSort.separar(List(2, 2, 2, 2, 2), 2, Nil, Nil), (List(), List(2, 2, 2, 2, 2)))
+  }
+
+  test("quickSort") {
+    assertEquals(QuickSort.quickSort(List()), List())
+    assertEquals(QuickSort.quickSort(List(5)), List(5))
+    assertEquals(QuickSort.quickSort(List(1, 2, 3, 4, 5)), List(1, 2, 3, 4, 5))
+    assertEquals(QuickSort.quickSort(List(5, 4, 3, 2, 1)), List(1, 2, 3, 4, 5))
+    assertEquals(QuickSort.quickSort(List(2, 1, 2, 1, 2)), List(1, 1, 2, 2, 2))
+  }
 
 }
