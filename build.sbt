a611fb5171ce4dd9a2a396ac2eb9f4e09ef0aa7d
Santiago Campo Garcia <santiagocampogarcia@gmail.com>
2026-09-25 17:18:04 -0500

    QuickSort functions and Munit dependency

--- build.sbt
+++ build.sbt
@@ -6,3 +6,4 @@ lazy val root = (project in file("."))
   .settings(
     name := "proyecto_integradora"
   )
+libraryDependencies += "org.scalameta" %% "munit" % "1.0.0" % Test
--- src/main/scala/QuickSort.scala
+++ src/main/scala/QuickSort.scala
@@ -1,3 +1,21 @@
 class QuickSort {
+  def menores(inputList: List[Int], p: Int): List[Int] = {
 
+  }
+
+  def mayores(inputList: List[Int], p: Int): List[Int] = {
+
+  }
+  
+  def appendTR(inputL1: List[Int], inputL2: List[Int]):List[Int] = {
+    
+  }
+
+  def separar(inputList: List[Int], p:Int, menoresAcc: List[Int], mayoresAcc: List[Int]): (List[Int], List[Int]) = {
+    
+  }
+  
+  def quickSort(inputList: List[Int]): List[Int] = {
+    
+  }
 }
