283250d12e2257c05106358f5f5cac3a948dfa6e
Onatejose <1120743000@u.icesi.edu.co>
2026-09-25 17:26:30 -0500

    Add QuickSort3

--- src/main/scala/QuickSort3.scala
+++ src/main/scala/QuickSort3.scala
@@ -1,4 +1,29 @@
 class QuickSort3 {
-  
+    def separar3(
+                  inputList: List[Int],
+                  p: Int,
+                  menoresAcc: List[Int],
+                  igualesAcc: List[Int],
+                  mayoresAcc: List[Int]
+                ): (List[Int], List[Int], List[Int]) =
+      inputList match
+        case Nil => (menoresAcc, igualesAcc, mayoresAcc)
+        case head :: tail =>
+          if head < p then
+            separar3(tail, p, head :: menoresAcc, igualesAcc, mayoresAcc)
+          else if head == p then
+            separar3(tail, p, menoresAcc, head :: igualesAcc, mayoresAcc)
+          else
+            separar3(tail, p, menoresAcc, igualesAcc, head :: mayoresAcc)
+
+    def quickSort3(inputList: List[Int]): List[Int] =
+      inputList match
+        case Nil => Nil
+        case p :: rest =>
+          val (menores, iguales, mayores) = separar3(rest, p, Nil, Nil, Nil)
+          QuickSort.appendTR(
+            quickSort3(menores),
+            QuickSort.appendTR(p :: iguales, quickSort3(mayores))
+          )
 }
 
